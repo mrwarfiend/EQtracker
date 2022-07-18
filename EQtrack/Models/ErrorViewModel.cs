5837be4c58f6ed8104dbbd98a4549653a993b907
Isaac Marsden <punisherqtp@gmail.com>
2022-07-18 16:25:18 -0400

    Initializing EQtracker Project

---
+++ EQtrack/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EQtrack.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
