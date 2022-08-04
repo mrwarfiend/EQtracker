5e195606653fb13d909947375296e82b7f9c3875
Isaac Marsden <punisherqtp@gmail.com>
2022-08-04 09:47:41 -0400

    Added stats page through home controller with react implementation

--- EQtrack/Controllers/HomeController.cs
+++ EQtrack/Controllers/HomeController.cs
@@ -29,5 +29,10 @@ namespace EQtrack.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult statistics()
+        {
+            return View();
+        }
     }
 }
---
+++ EQtrack/wwwroot/js/react/statistics/pages/Main.jsx
@@ -0,0 +1,24 @@
+﻿
+
+function Main() {
+
+    var paragraph = 'The versatility of EQ track can be demonstrated with its ability to work in tandem with a wide array of asset tracking technologies and systems. In 2019 various logistics companies globally were surveyed and asked what type of services they offered, and we found EQTrack is compatible & implementable with all processes, systems, and/or technologies.';
+
+    return (<div>
+        <div className="backBoard">
+            <div className="row">
+                <h1 className="title">Statistics</h1>
+            </div>
+            <div className="row">
+                <div className="col-xxl">
+                    <img className="image" src="/images/stats.png" alt="Stats" />
+                </div>
+                <div className="col-xxl">
+                    <h3 className="text">{paragraph}</h3>
+                </div>
+            </div>
+        </div>
+    </div>)
+}
+
+export default Main;
